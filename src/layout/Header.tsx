0a87e53b972d2b74473ea96dde4c77df925c4fd5
TeAnLi <m1055965862@163.com>
2025-06-12 02:11:32 +0800

    feat(component): update SideBar

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <div className="flex items-center w-full h-28 border-b-[1px] border-neutral-500">
       <InputField
-        className="ml-12 w-[29rem] h-10"
+        className="ml-12 mr-12 w-[29rem] h-10"
         placeholder="请输入关键词"
         onChange={(value) => {}}
       />
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const Sidebar: React.FC = () => {
+  const [activeItem, setActiveItem] = useState("Home");
   return (
-    <div className="flex flex-col p-2 bg-forgeground h-screen w-24 rounded-tr-2xl rounded-br-2xl">
-      <div></div>
+    <div className="flex flex-col items-center bg-forgeground h-screen w-28 rounded-tr-2xl rounded-br-2xl">
+      <div
+        onClick={() => {
+          setActiveItem("Home");
+        }}
+        className="mt-12 size-12 bg-slate-500 cursor-pointer"
+      ></div>
+      <div
+        onClick={() => {
+          setActiveItem("Setting");
+        }}
+        className="mt-12 size-12 bg-slate-500 cursor-pointer"
+      ></div>
+      <div
+        onClick={() => {
+          setActiveItem("Downloadd");
+        }}
+        className="mt-12 size-12 bg-slate-500 cursor-pointer"
+      ></div>
+      <div
+        onClick={() => {
+          setActiveItem("Null");
+        }}
+        className="mt-12 size-12 bg-slate-500 cursor-pointer"
+      ></div>
     </div>
   );
 };
