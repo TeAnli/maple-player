68de39e478d629d3de481d9dffe8606432d5abba
TeAnLi <m1055965862@163.com>
2025-06-15 15:55:00 +0800

    feat(window): remove window title bar

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,10 +3,11 @@ import Search from "../assets/Search.svg";
 import Cross from "../assets/Cross.svg"
 
 import { invoke } from "@tauri-apps/api/core";
+
 const Header: React.FC = () => {
   return (
     <div className="fixed w-full">
-      <div className="flex items-center h-24 border-b-[0.5px]">
+      <div data-tauri-drag-region className="flex justify-between items-center h-24 border-b-[0.5px]">
         <InputField
           className="ml-12 mr-12 w-96 h-10"
           placeholder="请输入关键词"
@@ -15,11 +16,11 @@ const Header: React.FC = () => {
         />
         <div onClick={async () => {
           invoke("exit");
-        }} className="right-0 hover:cursor-pointer">
+        }} className="hover:cursor-pointer mr-40">
           <img src={Cross} width={20} height={20}></img>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
