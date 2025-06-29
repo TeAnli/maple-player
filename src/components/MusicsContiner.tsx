afdcb85884adfb3ca820c1c98b8895c1f23b4b23
TeAnLi <m1055965862@163.com>
2025-06-29 23:35:30 +0800

    feat(component): Add MusicContiner component and account store

---
+++ src/components/MusicsContiner.tsx
@@ -0,0 +1,11 @@
+const MusicContiner: React.FC = () => {
+    return (
+        <div className="w-full h-full border border-gray-200/60 overflow-auto rounded-lg bg-white/5">
+            <div className="flex flex-col gap-4 p-4">
+
+            </div>
+        </div>
+
+    )
+}
+export default MusicContiner;
--- src/pages/views/User.tsx
+++ src/pages/views/User.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Folderlist from "../../components/Folderlist";
+import MusicContiner from "../../components/MusicsContiner";
 
 const User: React.FC = () => {
     return (
-        <div className="w-full h-full overflow-hidden p-4">
+        <div className="flex w-full h-full overflow-hidden p-4 gap-4">
             <Folderlist />
+            <MusicContiner />
         </div>
     );
 };
