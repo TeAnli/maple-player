cf357961292969093061fa6b7005d80fa8536638
TeAnLi <m1055965862@163.com>
2025-06-29 22:18:24 +0800

    chore: Update top margin

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -11,23 +11,21 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className="fixed bg-forgeground h-screen w-[80px]">
-      <div className="flex flex-col gap-8 items-center mt-24">
+      <div className="flex flex-col gap-8 items-center mt-8">
         {routes.map((item) => {
           const isActive = location.pathname === item.path;
           return (
             <div
               key={item.id}
-              className={`flex justify-center rounded-2xl w-12 h-12 cursor-pointer transition-all duration-300 ${
-                isActive
+              className={`flex justify-center rounded-2xl w-12 h-12 cursor-pointer transition-all duration-300 ${isActive
                   ? "bg-gradient-to-b from-secondary to-primary"
                   : "hover:bg-hover-primary hover:scale-110"
-              }`}
+                }`}
               onClick={() => handleItemClick(item.path)}
             >
               <img
-                className={`transition-all duration-500 ${
-                  isActive ? "opacity-100" : "opacity-30"
-                }`}
+                className={`transition-all duration-500 ${isActive ? "opacity-100" : "opacity-30"
+                  }`}
                 src={item.icon}
                 alt={item.label}
               />
