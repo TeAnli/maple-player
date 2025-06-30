e74906b3df20b96c67099830be39654a8ae56735
TeAnLi <m1055965862@163.com>
2025-07-01 01:37:53 +0800

    feat(store): Add persistent storage

--- src/store/account_store.ts
+++ src/store/account_store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { listen } from "@tauri-apps/api/event";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface AccountInfo {
   isLogin: boolean;
@@ -8,22 +9,28 @@ interface AccountInfo {
   setUID: (uid: number) => void;
 }
 
-export const useAccountStore = create<AccountInfo>()((set) => {
-  const initializeEventListeners = async () => {
-    await listen("login", (event) => {
-      const data = event.payload as { mid: number };
-      if (data) {
-        set(() => ({ isLogin: true, uid: 440078311 }));
-      }
-    });
-  };
-
-  initializeEventListeners();
-
-  return {
-    isLogin: false,
-    uid: null,
-    setLoginState: (isLogin: boolean) => set(() => ({ isLogin: isLogin })),
-    setUID: (uid: number) => set(() => ({ uid: uid })),
-  };
-});
+export const useAccountStore = create<AccountInfo>()(
+  persist(
+    (set, get) => {
+      const initializeEventListeners = async () => {
+        await listen("login", (event) => {
+          const data = event.payload as { mid: number };
+          if (data) {
+            set(() => ({ isLogin: true, uid: 440078311 }));
+          }
+        });
+      };
+      initializeEventListeners();
+      return {
+        isLogin: false,
+        uid: null,
+        setLoginState: (isLogin: boolean) => set(() => ({ isLogin: isLogin })),
+        setUID: (uid: number) => set(() => ({ uid: uid })),
+      };
+    },
+    {
+      name: "account_storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
