af4e8796abf5e380c2a57e9db8269534f326a9ed
TeAnLi <m1055965862@163.com>
2025-07-07 15:17:21 +0800

    feat: Set event setup loader

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,20 @@ import App from "./App";
 import "./main.css";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import { listen } from "@tauri-apps/api/event";
+async function setupEventListeners() {
+  console.log('正在装载监听事件..');
+  const unlistenDownloadProgress = await listen("download_progress", (event) => {
+    console.log(event);
+  });
+  console.log("事件监听装载完毕")
+  window.addEventListener('beforeunload', () => {
+    unlistenDownloadProgress();
+  });
+}
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
     <App />
   </Theme>
-
 );
+setupEventListeners().catch(console.error);
