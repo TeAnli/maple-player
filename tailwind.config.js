4ebb48cf6c7a3e36316b5417096aa6947eb3bfe7
TeAnLi <m1055965862@163.com>
2025-07-16 14:21:07 +0800

    style: Update theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // 添加深色模式支持
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#F4BA18",
-        secondary: "#F86E13",
-        background: "#343434",
-        forgeground: "#222222",
-        "font-primary": "rgba(24, 28, 47, 30%)",
-        "hover-primary": "rgba(255, 255, 255, 20%)",
+        primary: "rgb(var(--color-primary))",
+        secondary: "rgb(var(--color-secondary))",
+        content: "rgb(var(--color-content))",
+        foreground: "rgb(var(--color-foreground))",
+        card: "rgb(103 103 103)",
+        hovered: "rgb(70 70 70 / 0.7)",
+      },
+      textColor: {
+        primary: "rgb(var(--color-text-primary))",
       },
       boxShadow: {
         super: "0 20px 50px rgba(0,0,0, 0.10)",
@@ -20,6 +24,9 @@ module.exports = {
       },
       width: {
         sidebar: "120px",
+      },
+      height: {
+        header: "height: 2rem",
       },
       margin: {
         sidebar: "120px",
